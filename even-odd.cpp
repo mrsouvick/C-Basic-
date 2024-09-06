459c5e5dca86443316fb8c2f2f3a4b8f87b2b89c
Souvick Kumar Halder <reach.souvick@gmail.com>
2024-09-06 22:56:12 +0530

    file uploaded

---
+++ even-odd.cpp
@@ -0,0 +1,14 @@
+//Determine if a number is even or odd.
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"Enter the number:";
+cin>>num;
+if(num%2==0){
+    cout<<"EVEN";
+}else{
+    cout<<"ODD";
+}
+    return 0;
+}
